8618b1fbc70b58b31c133886cda96ce879743de6
MasterZeeno <zeenoliev@gmail.com>
2025-03-15 08:07:23 +0800

    Init

--- zee.sh
+++ zee.sh
@@ -7,4 +7,11 @@ MODDIR=${0%/*}
 CURRENT_VERSION=$(cat "$VERSION_FILE")
 
 check_dependencies curl unzip jq || exit 1
-update
+
+for type in monet black; do
+    REPO_TYPE="${type}-og"
+    ORIG_JSON_URL="$SITE/$ORIG_AUTHOR/$ORIG_REPO/main/$ORIG_REPO_ID/${REPO_TYPE}.json"
+    JSON_FILE="$MODDIR/$REPO/${REPO_TYPE}.json"
+    RELEASE_FILE="${REPO}-${REPO_TYPE}.zip"
+    update
+done
